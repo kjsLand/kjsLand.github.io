c8824b64d8dfa29142009d0ae43f2460cca96339
kjsLand <kevin.js.land@gmail.com>
2026-02-01 17:09:56 -0500

    Fixed config file for pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",          // makes static HTML
-  images: { unoptimized: true }
+const repo = "kjsLand.github.io";
+
+module.exports = {
+  output: "export",
+  images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
